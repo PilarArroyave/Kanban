8effb0ab261a686d9310fe4ffd03b4be66b1730e
Pilar Arroyave <pilar.arroyave@tenebit.com.co>
2020-10-26 20:16:10 -0500

    Actualizar archivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
-import { MainViewComponent } from './pages/main-view/main-view.component';
-import { NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { MainViewComponent } from './pages/main-view/main-view.component';
 
 const routes: Routes = [
   { path: '', component: MainViewComponent }
--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -43,16 +43,14 @@ export class MainViewComponent implements OnInit {
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex);
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      transferArrayItem(
-        event.previousContainer.data,
+      transferArrayItem(event.previousContainer.data,
         event.container.data,
         event.previousIndex,
         event.currentIndex);
     }
   }
+
+
 }
